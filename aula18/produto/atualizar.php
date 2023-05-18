0628eeafa00bf2610fdc435c64e40739c318930e
Maria Eduarda <dudabarboni2305@gmail.com>
2023-05-18 10:42:37 -0300

    foto

---
+++ aula18/produto/atualizar.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once "../conexao.php";
+
+if(isset($_POST["nome"])&& isset($_POST["descricao"])&& isset ($_POST["preco"])){
+$id = $_POST["id"];
+$nome = $_POST["nome"];
+$descricao = $_POST["descricao"];
+$preco = $_POST["preco"];
+
+//String com o comando SQL para ser executado no DB
+$sql = "UPDATE produto SET `nome`=?, `descricao`=?, `preco`=?, `foto`=? WHERE  `idproduto`=?;";
+
+
+
+//Prepara o SQL para ser executado no banco de dados
+$comando = $conexao->prepare($sql);
+
+//Adiciona os valores nos parâmetros
+$comando->bind_param("ssdsi", $nome, $descricao, $preco, $foto, $id);
+
+//Executa o SQL - Comando no Banco de Dados
+$comando->execute();
+}
+//abre o arquivo form.php
+header("Location: produtos.php");
